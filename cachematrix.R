54fd227ec14f9e3502439cd51c806c1fdbae8003
crumbofthypuss <mbevangelista4@up.edu.ph>
2020-11-02 21:59:29 +0800

    Changed the code in the forked assignment to the required code for inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function returns the inverse of the matrix provided but it can also
+## cache the previous value of the inverse if it has already been solved
 
-## Write a short comment describing this function
-
+## Caches the values of the matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inversematrix <- NULL
+  getmatrix <- function() x
+  setinverse <- function(inverse) inversematrix<<-inverse
+  getinverse <- function() inversematrix
+  list(getmatrix=getmatrix,setinverse=setinverse, getinverse=getinverse)
+  
 }
 
-
-## Write a short comment describing this function
-
+## solves for inverse of matrix, if inverse has been computed already, return cached value
+## initialmatrix=provided matrix, inversematrix= solved inverse of matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inversematrix<-x$getinverse()
+  if (!is.null(inversematrix)){
+    print("solved, already. Retrieving cached data....")
+    return(inversematrix)
+  }
+  initialmatrix<- x$getmatrix()
+  inversematrix<- solve(initialmatrix,...)
+  x$setinverse(inversematrix)
+  inversematrix
+  
 }
+
+#NOTE: WHEN DOING THIS HOMEWORK, FOLLOW THE STEPS BELOW
+#x<- matrix()
+#y<-makeCacheMatrix(x)
+#cacheSolve(y)
+#-----to verify if value was cached-------
+#cacheSolve(y)
+
